ed69d51beab175bbeaed69493f4dcbc68fe3d80f
RomaOnyshkiv <romaonyshkiv@gmail.com>
2017-01-05 22:57:38 +0200

    trivial fix

--- src/test/java/com/serenity/based/features/navigation/WhenBrowsingProductCategories.java
+++ src/test/java/com/serenity/based/features/navigation/WhenBrowsingProductCategories.java
@@ -34,6 +34,8 @@ public class WhenBrowsingProductCategories {
 
     @Test
     public void shouldBeAbleToNavigateToElectronicsCategory(){
+
+        //        Given
         mark.isOnTheHomePage();
 
         //        When
